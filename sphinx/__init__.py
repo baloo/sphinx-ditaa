cdfb369c2cd4bb7abad93feec61d61246e58dd10
Arthur <aga@zenexity.com>
2011-07-31 16:30:28 +0200

    First commit

---
+++ sphinx/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    zenexity-sphinx
+    ~~~~~~~~~~~~~~~
+
+    Contains Zenexity specific sphinx features not activated by default
+
+    :copyright: Copyright Arthur Gautier 2011
+    :copyright: Copyright Zenxity 2011
+    :licence: BSD
+"""
