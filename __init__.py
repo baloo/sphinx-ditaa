15e40098a78929f77b11a00bc8ee382c25ff8a68
Arthur <aga@zenexity.com>
2011-07-31 18:04:16 +0200

    Adds missing init file

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinx-ditaa
+    ~~~~~~~~~~~~~~~
+
+    Contains ditaa specific sphinx features
+
+    :copyright: Copyright Arthur Gautier 2011
+    :copyright: Copyright Zenxity 2011
+    :licence: BSD
+"""
